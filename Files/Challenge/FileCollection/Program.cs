19ad1e0be36b54cc5de1c0698c5316deaf3ea243
tasoftdev <163927935+tasoftdev@users.noreply.github.com>
2024-06-28 19:09:59 +0100

    Add files via upload

---
+++ Files/Challenge/FileCollection/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string directoryName = "FileCollection";
+        string resultsFileName = "results.txt";
+        
+        // Create the directory if it doesn't exist
+        Directory.CreateDirectory(directoryName);
+
+        // Initialize counters and size variables
+        int excelCount = 0, wordCount = 0, pptCount = 0;
+        long excelSize = 0, wordSize = 0, pptSize = 0;
+
+        // Create DirectoryInfo object
+        DirectoryInfo dirInfo = new DirectoryInfo(directoryName);
+
+        // Enumerate files in the directory
+        foreach (var file in dirInfo.EnumerateFiles())
+        {
+            if (IsOfficeFile(file))
+            {
+                // Update counters and sizes based on file type
+                switch (file.Extension.ToLower())
+                {
+                    case ".xlsx":
+                        excelCount++;
+                        excelSize += file.Length;
+                        break;
+                    case ".docx":
+                        wordCount++;
+                        wordSize += file.Length;
+                        break;
+                    case ".pptx":
+                        pptCount++;
+                        pptSize += file.Length;
+                        break;
+                }
+            }
+        }
+
+        // Write results to results.txt
+        using (StreamWriter writer = new StreamWriter(Path.Combine(directoryName, resultsFileName)))
+        {
+            writer.WriteLine("Summary of Office files in directory: " + directoryName);
+            writer.WriteLine($"Excel files: {excelCount}, Total size: {FormatSize(excelSize)}");
+            writer.WriteLine($"Word files: {wordCount}, Total size: {FormatSize(wordSize)}");
+            writer.WriteLine($"PowerPoint files: {pptCount}, Total size: {FormatSize(pptSize)}");
+            writer.WriteLine($"Total Office files: {excelCount + wordCount + pptCount}");
+            writer.WriteLine($"Total size of Office files: {FormatSize(excelSize + wordSize + pptSize)}");
+        }
+
+        Console.WriteLine("Summary written to " + resultsFileName);
+    }
+
+    static bool IsOfficeFile(FileInfo file)
+    {
+        string[] officeExtensions = { ".xlsx", ".docx", ".pptx" };
+        return officeExtensions.Contains(file.Extension.ToLower());
+    }
+
+    static string FormatSize(long bytes)
+    {
+        string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+        double len = bytes;
+        int order = 0;
+        while (len >= 1024 && order < sizes.Length - 1)
+        {
+            order++;
+            len = len / 1024;
+        }
+        return String.Format("{0:0.##} {1}", len, sizes[order]);
+    }
+}
---
+++ Files/FileInfo/Program.cs
@@ -0,0 +1,54 @@
+﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
+// Reading and Writing data from and to files
+
+// Make sure the example file exists
+const string filename = "TestFile.txt";
+
+// TODO 1: WriteAllText overwrites a file with the given content
+if (!File.Exists(filename)) {
+    File.WriteAllText(filename, "This is a text file. ");
+}
+
+// TODO 3: AppendAllText adds text to an existing file
+File.AppendAllText(filename, "This text gets appended to the file. ");
+
+// TODO 4: A FileStream can be opened and written to until closed
+using (StreamWriter sw = File.AppendText(filename)) {
+sw.WriteLine("Line 1");
+sw.WriteLine("Line 2");
+sw.WriteLine("Line 3");
+}
+
+
+// TODO 2: ReadAllText reads all the content from a file
+// Application Programming .NET Programming with C# by Abdullahi Tijjani
+// Working with file information
+
+// Make sure the example file exists
+if (!File.Exists(filename)) {
+    using (StreamWriter sw = File.CreateText(filename)) {
+        sw.WriteLine("This is a text file.");
+    }
+}
+
+// TODO: Get some information about the file
+Console.WriteLine("File creation time was: "+ File.GetCreationTime(filename));
+Console.WriteLine("File last write time was: "+File.GetLastWriteTime(filename));
+Console.WriteLine("File last access tme was: "+File.GetLastAccessTime(filename));
+
+// TODO: We can also get general information using a FileInfo 
+try{
+    FileInfo fi = new FileInfo(filename);
+    System.Console.WriteLine($"{fi.Length}");
+}
+catch (Exception e){
+    Console.WriteLine($"Error: {e}");
+}
+
+// TODO: File information can also be manipulated
+File.SetAttributes(filename,FileAttributes.ReadOnly);
+Console.WriteLine(File.GetAttributes(filename));
+
+DateTime dt = new DateTime(2024,6,10);
+File.SetCreationTime(filename, dt);
+Console.WriteLine(File.GetCreationTime(filename));
